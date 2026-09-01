a9a1c2bd5f8a3d1a1e0c59dcae4545c6f4b8e9f5
Ítalo Felipe <italofelipe0202@gmail.com>
2026-09-01 15:39:57 -0300

    multireciclagem feito

--- src/Entidades/MutiraoReciclagem.java
+++ src/Entidades/MutiraoReciclagem.java
@@ -1,4 +1,41 @@
 package Entidades;
 
-public class MutiraoReciclagem {
+public class MutiraoReciclagem extends Acao {
+
+    private int duracaoHoras;
+
+    public MutiraoReciclagem(int id, String titulo, String descricao, String data, int maxParticipantes, int duracaoHoras){
+        super(id, titulo, descricao, data, maxParticipantes);
+        this.duracaoHoras=duracaoHoras;
+    }
+    @Override
+    public int calcularPontuacao() {
+        int pontos = duracaoHoras * 3;
+
+        if (kitMaterial) {
+            pontos += 10;
+        }
+        return pontos;
+    }
+
+    public int getDuracaoHoras() {
+        return duracaoHoras;
+    }
+    public void setDuracaoHoras(int duracaoHoras){
+        this.duracaoHoras = duracaoHoras;
+    }
+    public boolean isKitMaterial() {
+        return kitMaterial;
+    }
+    public void setKitMaterial(boolean kitMaterial) {
+        this.kitMaterial = kitMaterial;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "\nTipo: Oficina Ecológica" +
+                "\nDuração: " + duracaoHoras + " horas" +
+                "\nKit de material: " + (kitMaterial ? "Sim" : "Não");
+    }
 }
