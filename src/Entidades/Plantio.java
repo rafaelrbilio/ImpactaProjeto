ec3f3a333a74f3eef7c1bd630509ab92cb59197c
rafaelAraujofac <rafaelrbilio@gmail.com>
2026-09-01 16:06:55 -0300

    classe Plantio

--- src/Entidades/Plantio.java
+++ src/Entidades/Plantio.java
@@ -1,4 +1,31 @@
 package Entidades;
 
-public class Plantio {
+public class Plantio extends Acao{
+    private int qntMudas;
+
+    public Plantio(int id, String titulo, String descricao, String data, int maxParticipantes, int qntMudas) {
+        super(id, titulo, descricao, data, maxParticipantes);
+        this.qntMudas = qntMudas;
+    }
+
+    public int getQntMudas() {
+        return qntMudas;
+    }
+
+    public void setQntMudas(int qntMudas) {
+        this.qntMudas = qntMudas;
+    }
+
+    @Override
+    public int calcularPontuacao() {
+
+        return 5 + (2*qntMudas);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "\nTipo: Plantio de Mudas" +
+                "\nQuantidade de mudas: " + qntMudas;
+    }
 }
