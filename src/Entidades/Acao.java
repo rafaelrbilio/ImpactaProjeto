96add6864296914e1d89c045c85294e07b448f69
rafaelAraujofac <rafaelrbilio@gmail.com>
2026-09-01 15:21:02 -0300

    classe abstrata acao

--- src/Entidades/Acao.java
+++ src/Entidades/Acao.java
@@ -1,4 +1,59 @@
 package Entidades;
 
-public class Acao {
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Acao {
+    protected int id;
+    protected String titulo;
+    protected String descricao;
+    protected String data;
+    protected int maxParticipantes;
+    protected List<Voluntario> inscritos;
+
+    public Acao(int id, String titulo, String descricao, String data, int maxParticipantes) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.data = data;
+        this.maxParticipantes = maxParticipantes;
+        this.inscritos = new ArrayList<>();
+    }
+
+    public abstract int calcularPontuacao();
+
+    public boolean adicionarVoluntario(Voluntario voluntario){
+        if(inscritos.size()>=maxParticipantes){
+            return false;
+        }
+        if(inscritos.contains(voluntario)){
+            return false;
+        }
+        inscritos.add(voluntario);
+        return true;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public int getMaxParticipantes() {
+        return maxParticipantes;
+    }
+
+    public List<Voluntario> getInscritos() {
+        return inscritos;
+    }
 }
