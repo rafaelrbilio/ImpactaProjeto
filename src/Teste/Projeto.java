77c5709b36c13058a6313d37330045164543122b
Ítalo Felipe <italofelipe0202@gmail.com>
2026-09-01 15:00:56 -0300

    criei os package

--- src/Teste/Projeto.java
+++
@@ -1,7 +0,0 @@
-package Teste;
-
-public class Projeto {
-    static void main(String[] args) {
-        System.out.println("Rafael codigo ajeitado paulo italo rafael dnovo raiff gayfael  hd");
-    }
-}
